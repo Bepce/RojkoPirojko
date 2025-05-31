fa9ca5b269e059aee7a553eb7e073592b0291a4f
Bepce <bginfernali@gmail.com>
2025-05-31 13:38:28 +0300

    Trying to get this thing to deploy
    please work

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './App.css'
 import './components/Navbar.css'
 import './components/Home.css'
-import App from './App.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
